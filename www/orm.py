8b2e679c061fd2571dbf8c0ff4e0809dc70bc32a
clairehu <471339423@qq.com>
2017-07-16 12:47:32 +0800

    Model

--- www/orm.py
+++ www/orm.py
@@ -1,89 +1,236 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__author__ = 'claire hu'
+__author__ = 'Michael Liao'
 
-import  logging,aiomysql
-# 创建全局的连接池，每个 HTTP 请求都可以从连接池中直接获取数据库连接
-# 使用连接池的好处是不用频繁地打开和关闭数据库连接，而是能复用就尽量复用。
+import asyncio, logging
 
-# async def create_pool(loop, **kw):
-#     logging.info("create database connection pool...")
-#     # 连接池由全局变量__pool 存储，缺省情况下将编码设置为 utf8，自动提交
-#     global __pool
-#     __pool = await from aiomysql.create(
-#     host = kw.get("host","localhost"),
-#     port = kw.get("port",3306),
-#     user = kw["user"],
-#     password=kw["password"],
-#     db = kw["db"],
-#     charset = kw.get("charset","utf8"),
-#     autocommit=kw.get("autommit",True),
-#     maxsize = kw.get("maxsize",10),
-#     minsize = kw.get("minsize",1),
-#     loop = loop
-#     )
+import aiomysql
 
-    # 创建一个全局的连接池，每个http请求都从池中获得数据库连接
-    async def create_pool(loop, **kw):
-        logging.info('create data base connection pool......')
-        # 全局__pool用于存储整个连接池
-        global __pool
-        __pool = await aiomysql.create_pool(
-            # **kw参数可以包含所有连接需要用到的关键字参数
-            # 默认本机IP
-            host=kw.get('host', 'localhost'),
-            port=kw.get('port', 3306),
-            user=kw['user'],
-            password=kw['password'],
-            db=kw['db'],
-            charset=kw.get('charset', 'utf8'),  # 注意是utf8
-            autocommit=kw.get('autocommit', True),
-            maxsize=kw.get('maxsize', 10),
-            minsize=kw.get('minsize', 1),
+def log(sql, args=()):
+    logging.info('SQL: %s' % sql)
 
-            # 接收一个event_loop实例
-            loop=loop
-            dheiadhe
-            da fe] floata
-        dbwu fbuw abs(
-            dbfesjdhue
-            d fbdwnai
-        feai
-        feao safij
-        dfeia
-        fwanisa' fiwa ' \
-               'fsa' \
-               ' fjwa' \
-               ' ' \
-               '' \
-               'a fnsia fnwei ' \
-               'ndifn gn gs ' \
-               '' \
-               '' \
-               'AHFEHFSNFKDNSKAEI ' \
-               'FA GNS KFA' BG
-         FNSA IIEF VA FSAKHFSAE FAEIF
-        AE FalseA FNIEW AFANIF
-        EFNWIA HFISA IFW
-         FSAI FWIA
-        FSA
-         FHWI AFSIA
-         FWA FNSA FJIWA
-        FSA FSA FNSAD NFIw hfisa hfis afhi hsia fhis abs(fs
-                                                         a fsa
-        sa fsa fhsia if
-        a fnwa fsa' ' \
-                  'f' \
-                  'wa fosa hfiea ' \
-                  'fhisa fhie a' \
-                  'fewafh ifhea ' \
-                  'fa fiew fheiwa hdfisa ' \
-                  'fs' \
-                  'a efhia  fnkaw fnsda 发吧和vhfweffhy8f84t 恢复为合肥市风好大 fw8发 g 撒防护网个范围 vfgsuafaafewgaf 埃尔功夫干撒 vhfi 而已 发 f 撒功夫无咖妃啊幅度变白发故事的噶' \
-                  '额' \
-                  'fa 发' \
-                  'fe'wa')
-        )
-        )
+# 创建一个全局的连接池，每个 HTTP 请求都可以从连接池中直接获取数据库连接，使用连接池的好处是不必频繁打开关闭数据库连接，而是尽量复用
+async def create_pool(loop, **kw):
+    logging.info('create database connection pool...')
+    global __pool
+    __pool = await aiomysql.create_pool(
+        host=kw.get('host', 'localhost'),
+        port=kw.get('port', 3306),
+        user=kw['user'],
+        password=kw['password'],
+        db=kw['db'],
+        charset=kw.get('charset', 'utf8'),
+        autocommit=kw.get('autocommit', True),
+        maxsize=kw.get('maxsize', 10),
+        minsize=kw.get('minsize', 1),
+        loop=loop
+    )
+
+async def select(sql, args, size=None):
+    log(sql, args)
+    global __pool
+    async with __pool.get() as conn:
+        # DictCursor 表示返回为字典表示的记录
+        async with conn.cursor(aiomysql.DictCursor) as cur:
+            # execute 执行单条sql语句，将传入的字符串当做命令执行
+            #  SQL语句的占位符是?，而MySQL的占位符是%s，需要替换
+            await cur.execute(sql.replace('?', '%s'), args or ())
+            # 如果传入size参数，就通过 fetchmany() 获取最多指定数量的记录，否则，通过 fetchall() 获取所有记录
+            if size:
+                rs = await cur.fetchmany(size)
+            else:
+                rs = await cur.fetchall()
+        logging.info('rows returned: %s' % len(rs))
+        return rs
+
+# execute() 函数和 select() 函数所不同的是，cursor 对象不返回结果集，而是通过 rowcount 返回结果数。
+async def execute(sql, args, autocommit=True):
+    log(sql)
+    async with __pool.get() as conn:
+        if not autocommit:
+            await conn.begin()
+        try:
+            async with conn.cursor(aiomysql.DictCursor) as cur:
+                await cur.execute(sql.replace('?', '%s'), args)
+                affected = cur.rowcount
+            if not autocommit:
+                await conn.commit()
+        except BaseException as e:
+            if not autocommit:
+                await conn.rollback()
+            raise
+        return affected
+
+def create_args_string(num):
+    L = []
+    for n in range(num):
+        L.append('?')
+    return ', '.join(L)
+
+class Field(object):
+
+    def __init__(self, name, column_type, primary_key, default):
+        self.name = name
+        self.column_type = column_type
+        self.primary_key = primary_key
+        self.default = default
+
+    def __str__(self):
+        return '<%s, %s:%s>' % (self.__class__.__name__, self.column_type, self.name)
+
+class StringField(Field):
+
+    def __init__(self, name=None, primary_key=False, default=None, ddl='varchar(100)'):
+        super().__init__(name, ddl, primary_key, default)
+
+class BooleanField(Field):
+
+    def __init__(self, name=None, default=False):
+        super().__init__(name, 'boolean', False, default)
+
+class IntegerField(Field):
+
+    def __init__(self, name=None, primary_key=False, default=0):
+        super().__init__(name, 'bigint', primary_key, default)
+
+class FloatField(Field):
+
+    def __init__(self, name=None, primary_key=False, default=0.0):
+        super().__init__(name, 'real', primary_key, default)
+
+class TextField(Field):
+
+    def __init__(self, name=None, default=None):
+        super().__init__(name, 'text', False, default)
+
+class ModelMetaclass(type):
+
+    def __new__(cls, name, bases, attrs):
+        if name=='Model':
+            return type.__new__(cls, name, bases, attrs)
+        tableName = attrs.get('__table__', None) or name
+        logging.info('found model: %s (table: %s)' % (name, tableName))
+        mappings = dict()
+        fields = []
+        primaryKey = None
+        for k, v in attrs.items():
+            if isinstance(v, Field):
+                logging.info('  found mapping: %s ==> %s' % (k, v))
+                mappings[k] = v
+                if v.primary_key:
+                    # 找到主键:
+                    if primaryKey:
+                        raise StandardError('Duplicate primary key for field: %s' % k)
+                    primaryKey = k
+                else:
+                    fields.append(k)
+        if not primaryKey:
+            raise StandardError('Primary key not found.')
+        for k in mappings.keys():
+            attrs.pop(k)
+        escaped_fields = list(map(lambda f: '`%s`' % f, fields))
+        attrs['__mappings__'] = mappings # 保存属性和列的映射关系
+        attrs['__table__'] = tableName
+        attrs['__primary_key__'] = primaryKey # 主键属性名
+        attrs['__fields__'] = fields # 除主键外的属性名
+        attrs['__select__'] = 'select `%s`, %s from `%s`' % (primaryKey, ', '.join(escaped_fields), tableName)
+        attrs['__insert__'] = 'insert into `%s` (%s, `%s`) values (%s)' % (tableName, ', '.join(escaped_fields), primaryKey, create_args_string(len(escaped_fields) + 1))
+        attrs['__update__'] = 'update `%s` set %s where `%s`=?' % (tableName, ', '.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f), fields)), primaryKey)
+        attrs['__delete__'] = 'delete from `%s` where `%s`=?' % (tableName, primaryKey)
+        return type.__new__(cls, name, bases, attrs)
+
+class Model(dict, metaclass=ModelMetaclass):
+
+    def __init__(self, **kw):
+        super(Model, self).__init__(**kw)
+
+    def __getattr__(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Model' object has no attribute '%s'" % key)
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+    def getValue(self, key):
+        return getattr(self, key, None)
+
+    def getValueOrDefault(self, key):
+        value = getattr(self, key, None)
+        if value is None:
+            field = self.__mappings__[key]
+            if field.default is not None:
+                value = field.default() if callable(field.default) else field.default
+                logging.debug('using default value for %s: %s' % (key, str(value)))
+                setattr(self, key, value)
+        return value
+
+    @classmethod
+    async def findAll(cls, where=None, args=None, **kw):
+        ' find objects by where clause. '
+        sql = [cls.__select__]
+        if where:
+            sql.append('where')
+            sql.append(where)
+        if args is None:
+            args = []
+        orderBy = kw.get('orderBy', None)
+        if orderBy:
+            sql.append('order by')
+            sql.append(orderBy)
+        limit = kw.get('limit', None)
+        if limit is not None:
+            sql.append('limit')
+            if isinstance(limit, int):
+                sql.append('?')
+                args.append(limit)
+            elif isinstance(limit, tuple) and len(limit) == 2:
+                sql.append('?, ?')
+                args.extend(limit)
+            else:
+                raise ValueError('Invalid limit value: %s' % str(limit))
+        rs = await select(' '.join(sql), args)
+        return [cls(**r) for r in rs]
+
+    @classmethod
+    async def findNumber(cls, selectField, where=None, args=None):
+        ' find number by select and where. '
+        sql = ['select %s _num_ from `%s`' % (selectField, cls.__table__)]
+        if where:
+            sql.append('where')
+            sql.append(where)
+        rs = await select(' '.join(sql), args, 1)
+        if len(rs) == 0:
+            return None
+        return rs[0]['_num_']
+
+    @classmethod
+    async def find(cls, pk):
+        ' find object by primary key. '
+        rs = await select('%s where `%s`=?' % (cls.__select__, cls.__primary_key__), [pk], 1)
+        if len(rs) == 0:
+            return None
+        return cls(**rs[0])
+
+    async def save(self):
+        args = list(map(self.getValueOrDefault, self.__fields__))
+        args.append(self.getValueOrDefault(self.__primary_key__))
+        rows = await execute(self.__insert__, args)
+        if rows != 1:
+            logging.warn('failed to insert record: affected rows: %s' % rows)
+
+    async def update(self):
+        args = list(map(self.getValue, self.__fields__))
+        args.append(self.getValue(self.__primary_key__))
+        rows = await execute(self.__update__, args)
+        if rows != 1:
+            logging.warn('failed to update by primary key: affected rows: %s' % rows)
+
+    async def remove(self):
+        args = [self.getValue(self.__primary_key__)]
+        rows = await execute(self.__delete__, args)
+        if rows != 1:
+            logging.warn('failed to remove by primary key: affected rows: %s' % rows)
 
